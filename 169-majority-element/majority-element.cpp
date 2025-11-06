9bf6a0bc3316fec3b1385bce9d72b243fefac813
Vatsal Sharma <156673638+vatsalsharmaaa@users.noreply.github.com>
2025-11-06 23:29:49 +0530

    Optimize majorityElement function implementation
    Refactor majority element function to use Boyer-Moore Voting Algorithm for improved efficiency.

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,29 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> freq;
+//         int n = nums.size();
+
+//         for (int num : nums) {
+//             freq[num]++;
+//             if (freq[num] > n / 2)
+//                 return num;  // found majority
+//         }
+//         return -1; // in theory won't happen since majority always exists
+//     }
+// };
+//Boyer-Moore Voting Algorithm (Most Efficient)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int n = nums.size();
+        int count = 0, candidate = 0;
 
         for (int num : nums) {
-            freq[num]++;
-            if (freq[num] > n / 2)
-                return num;  // found majority
+            if (count == 0)
+                candidate = num;
+            count += (num == candidate) ? 1 : -1;
         }
-        return -1; // in theory won't happen since majority always exists
+
+        return candidate;
     }
 };
